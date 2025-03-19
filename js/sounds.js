9be53e77ad5cb476d640b822fc94f6633e6fdfa9
Preetkomal533 <karrakomal56@gmail.com>
2025-03-18 18:56:46 -0700

    hii

--- js/sounds.js
+++ js/sounds.js
@@ -3,20 +3,18 @@ sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong']
 sounds.forEach(sound => {
      btn = document.createElement('button')
     btn.classList.add('btn')
-    btn.innerText= sound
+    btn.innerText=sound
     btn.addEventListener('click',() => {
         stopSongs()
         document.getElementById(sound).play()
 
      })
-
-
-    document.getElementById('buttons').appendChild(btn)
+     document.getElementById('buttons').appendChild(btn)
 
 })
 
-function stopSongs () {
-    sounds.forEach(sounds => {
+function stopSongs() {
+    sounds.forEach(sound => {
     song = document.getElementById(sound)
     song.pause()
     song.currentTime = 0
