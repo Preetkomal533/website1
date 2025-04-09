09dcb3af97fde86b63346ea1aad9e5d6540eb6e6
Preetkomal533 <karrakomal56@gmail.com>
2025-04-09 16:29:06 +0000

    Wed, Apr 9, 2025, 9:29 AM -07:00

--- js/ball.js
+++ js/ball.js
@@ -57,7 +57,7 @@ function moveBall() {
         (ballXDirection == -1)
     ){
         ballXDirection = ballXDirection * -1
-
+        score1 = score1 + 1
     }
 
 }
@@ -157,14 +157,9 @@ function createScore() {
     score.style.fontSize = "30px"
     score.style.textAlign = "center"
     score.style.justifyContent = "center"
-    let ballTop = ballYPosition
-    let ballBottom = ballYPosition + 2 * ballRadius
-    let ballLeft = ballXPosition
-    let LPadelTop = LPadelYPosition
-    let LPadelRight = LPadelXPosition + LPadelWidth
-    let LPadelBottom = LPadelYPosition + LPadelHeight
 
 }
+
 
 
 
